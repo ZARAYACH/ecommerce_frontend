428828139449b5438567afe8f439b2fa5b9cb632
MohammedChbani <mohammed.chbani@cprofile.nl>
2022-05-29 20:33:36 +0100

    the productImage now working properly and some improvements

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <img src="../../../../images/products/69c9f672-57d4-4041-b634-22651b233478.jpeg" className="App-logo" alt="logo" />
+
   );
 }
 
 export default App;
+
+

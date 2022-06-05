288efc2f88aa65347c4cb1d70b62fb2d9e5fa18a
MohammedChbani <mohammed.chbani@cprofile.nl>
2022-06-05 19:23:15 +0100

    the login proccess is working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import {BrowserRouter as Router,Route, Routes ,Link} from 'react-router-dom';
 import Home from './component/home/home';
+import Login from './component/login/login';
 
 
 
@@ -11,7 +12,8 @@ function App() {
    <div className='container'>
       <Router>
          <Routes>
-         <Route path='/' element= {<Home/>} />
+            <Route path='/' element= {<Home/>} />
+            <Route path='/account' element={<Login/>} />
          </Routes>
       </Router>
       
---
+++ src/component/login/login.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  axiosInstancePublic
+} from "../axiosConfig/axiosInstance";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailTemp = useRef();
+  const passwordTemp = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (password != "") {
+      axiosInstancePublic
+        .post("/login", {
+          email: email,
+          password: password,
+        })
+        .then((res) => res.data)
+        .then((data) => {
+          localStorage.setItem("t_access_token", data.access_token);
+          localStorage.setItem("t_refrech_token", data.refresh_token);
+          navigate("/dashbord");
+        })
+        .catch((err) => {
+          emailTemp.current.classList.add("error");
+          passwordTemp.current.classList.add("error");
+        });
+    }
+  }, [password]);
+
+  return (
+    <div className="login-wraper">
+      <div className="login-container outline-border">
+        <div className="container-head">
+          <Link to={"/"} className="logo">
+            T
+          </Link>
+        </div>
+        <div className="main-form">
+          <input
+            ref={emailTemp}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+          <input
+            ref={passwordTemp}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <div className="checkbox">
+            <input defaultChecked type="checkbox" name="stay" id="stay" />
+            <label htmlFor="stay">Stay signed in</label>
+          </div>
+          <input
+            onClick={() => {
+              setEmail(emailTemp.current.value);
+              setPassword(passwordTemp.current.value);
+            }}
+            type="button"
+            value="Login"
+          />
+        </div>
+        <div className="forgot">
+          <Link to={"/forgot"}>Did You forgot your password</Link>
+          <Link to={"/signup"}>You don't have an Account ?</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
